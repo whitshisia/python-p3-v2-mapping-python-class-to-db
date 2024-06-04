991575ec5acac2a71d28ef1e4b78d69325a7929c
whitney-shisia <shisiawhitney215@gmail.com>
2024-06-04 11:35:16 +0300

    py

--- lib/debug.py
+++ lib/debug.py
@@ -5,5 +5,23 @@ from __init__ import CONN, CURSOR
 from department import Department
 
 import ipdb
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)  
+
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)  
+
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)
+  
+print("Delete Payroll")
+payroll.delete()  
+print(payroll)  
 
 ipdb.set_trace()
